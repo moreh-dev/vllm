982310fa00e4d3d372b227ed779c1f5cb1372743
Jungwook Kim <iwooook@gmail.com>
2025-12-01 04:06:30 +0000

    feat: Add `pip uninstall vllm` before installation to ensure a clean install

--- install_vllm_tt.sh
+++ install_vllm_tt.sh
@@ -4,4 +4,5 @@ source /home/jungwook/tt-metal_moreh/python_env/bin/activate
 
 export VLLM_TARGET_DEVICE="tt"
 
+pip uninstall -y vllm
 pip install -e . --extra-index-url https://download.pytorch.org/whl/cpu
